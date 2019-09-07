c349f09b2d8cefe202a420a21623bcb3427f9e8f
waspryo <wasp2123321@gmail.com>
2019-09-07 20:41:17 +0900

    final implement

--- src/components/Game/index.js
+++ src/components/Game/index.js
@@ -2,50 +2,69 @@ import React, { useState } from "react";
 import "./style.css";
 import Box from "../Box/index";
 
+const board = [[], [], []];
+
 function Game() {
   const [turn, setTurn] = useState("x");
-  const board = [[], [], []]
+  const [winningtext, setWinningText] = useState('')
+  
 
   function changeTurn(row, col) {
-      board[row][row] = turn
-      setTurn(turn => turn === 'x' ? 'o' : 'x'  )
+    board[row][col] = turn;
+    setTurn(turn => (turn === "x" ? "o" : "x"));
     //   console.log(board)
 
-    checkForWin()
+    const winner = checkForWin();
+
+    if (!winner) {
+      // nothing
+    } else {
+        setWinningText(winner + ' won!')
+    }
   }
 
   function checkForWin() {
-      // row test
-      for(let i=0;i<board.length;i++) {
-          const row = board[i]
-          if(row[0] === row[1] && row[1] === row[2] && row[0]) {
-            return true
-          }
+    // row test
+    for (let i = 0; i < board.length; i++) {
+      const row = board[i];
+      if (row[0] === row[1] && row[1] === row[2] && row[0]) {
+        return row[0];
       }
+    }
 
-      // column test
-      for(let i=0;i<board.length;i++) {
-          const el1 = board[0][i]
-          const el2 = board[1][i]
-          const el3 = board[2][i]
+    // column test
+    for (let i = 0; i < board.length; i++) {
+      const el1 = board[0][i];
+      const el2 = board[1][i];
+      const el3 = board[2][i];
 
-          if(el1 === el2 && el2 === el3 && el1) {
-              return true
-          }
+      if (el1 === el2 && el2 === el3 && el1) {
+        return el1;
       }
+    }
     // diagonal test
 
-    const d1 = board[0][1]
-    const d2 = board[0][1]
-    const d3 = board[0][1]
+    const d1 = board[0][1];
+    const d2 = board[1][1];
+    const d3 = board[2][1];
 
-    if(d1 === d2 && d2 === d3 && d1) {
-        return true
+    if (d1 === d2 && d2 === d3 && d1) {
+      return d1;
     }
+
+    const p1 = board[0][2];
+    const p2 = board[1][1];
+    const p3 = board[2][0];
+
+    if (p1 === p2 && p2 === p3 && p1) {
+      return p1;
+    }
+    return false;
   }
 
   return (
     <div id="game">
+        <div id="winning-text">{winningtext}</div>
       <div className="row">
         <Box row={0} col={0} currentState={turn} changeTurn={changeTurn} />
         <Box row={0} col={1} currentState={turn} changeTurn={changeTurn} />
